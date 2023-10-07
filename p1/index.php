b629c44dbf20d467ec330f61aeffef616ad15299
Mary Bradley <mbradley@hbs.edu>
2023-10-07 04:08:48 +0000

    game logic v.1

--- p1/index.php
+++ p1/index.php
@@ -1,4 +1,20 @@
 <?php
+## Game planning
++ itialize an array called schedule for two elements, a string and an integer (band names and times)
++ Create an array of band names
++ Create an array of times to play
++ For performer, randomly choose an element from the band names 
++ For performer, randomly choose an element from the time to play array
++ Compare Performer A's time to the integer element in the schedule array, if they don't match, pop the band name and time to array 
++else, tell Performer to pick a new time.
++ if band name key >= 0 
++ For performer, randomly choose an element from the band names 
++ else Report band names and time they will play
+
+
+
+require 'index-view.php';
+
 //comment out code
 # Define 4 different variables, which will
 # each represent how much a given coin is worth
@@ -15,5 +31,3 @@
 
 # Add up how much money is in the piggy bank
 #$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value) + ($hquarters * $hquarter_value);
-
-require 'index-view.php';
