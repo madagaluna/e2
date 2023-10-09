d6948f46298d58ecb9befac53524164a326d1d06
Mary Bradley <mbradley@hbs.edu>
2023-10-09 21:39:32 +0000

    probably am going to kills some 'echo' darlings.

--- p1/index.php
+++ p1/index.php
@@ -1,6 +1,6 @@
 <?php
 ## Game planning
-// iniatiaze an array to hold results
+// iniatize an array to hold results
 $results = [];
 for ($i = 1; $i <=10; $i++) {
 //+ itialize an array called schedule
@@ -18,20 +18,18 @@ $performerB_time = 0;
     $performerB_time = $times[array_rand($times)];
 // compare performer B's time to performer A's time to see if time is available
         if ($performerB_time != $performerA_time) { 
-// updated schedule to include PB if the time is available and announce when both bands will perform  
-   
+// updated schedule to include PB if the time is available and announce when both bands will perform  this redundant now that I'm doing the results but I like it.  
         $schedule[] = $performerB_time;
-            echo "Rock and Roll! The Foos will play at  " . $performerA_time . " and The Bars will play at  " . $performerB_time ;
-// announce that the second band will not perform          
+         //   echo "Rock and Roll! The Foos will play at  " . $performerA_time . " and The Bars will play at  " . $performerB_time ;
+// announce that The Bars will not perform - maybe just keep this         
         } else {
                 echo "Wah-Wah is not just a pedal. The Bars will not be a performing today.";
             }
 
 // print the schedule https://www.w3schools.com/php/func_string_implode.asp
 echo "  There will be music at:  ". implode(" , ", $schedule);
-//$results[
- //   'The Foos' => $performerA_time,
-// ]
+//show results
+
    $results [] = [
     'The Foos time' => $performerA_time,
     'The Bars time' => $performerB_time,
