5533586488bb911f6652dcf8665ad2467d72aee1
Mary Bradley <mbradley@hbs.edu>
2023-10-09 14:51:08 +0000

    Simplify!! take out band name array

--- p1/index.php
+++ p1/index.php
@@ -4,17 +4,20 @@
 $schedule = ['band'. 'time'];
 //+ Create an array of band names with the names
 $band = ['The Woo', 'The Wah', 'The Wee', 'The Wink', 'Foo'];
+var_dump($band);
 //+ Create an array of times to play with the times
-$times[0,4];
+$times =[0,4];
 var_dump($times);
 //+ For performer, randomly choose an element from the band names 
-$performer1 = $band[rand(0,4), $times[rand(0.4)];
-var_dump($performer1);
+$performer = $band[rand(0,4)];
+var_dump($performer);
 //+ For performer, randomly choose an element from the time to play array
-//$performer1 = $times[rand(0-4)];
-
-//var_dump($performer1);
+$ = $times[rand(0,4)];
+var_dump($performer);
 //+ Compare Performer A's time to the integer element in the schedule array, if they don't match, pop the band name and time to array 
+if $performer[$time] != $schedule[1];
+$schedule = [$band, $times];
+else echo $performer[$time] "is not available.  Choose a new time';
 
 //+else, tell Performer to pick a new time.
 //+ if band name key >= 0 
