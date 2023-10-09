f518abe0d5ca1a4e4f5a7bf355121c34ff70e2b8
Mary Bradley <mbradley@hbs.edu>
2023-10-09 19:49:41 +0000

    clean up comments

--- p1/index.php
+++ p1/index.php
@@ -5,21 +5,21 @@
 $schedule = [];
 //+ Create an array of times to play with the times
 $times =[1,2,3];
+//defining the performer variables
 $performerA_time = 0;
 $performerB_time = 0;
-//var_dump($times);
-//+ For Performer A, randomly choose a time to play:
+//+ For Performer A, randomly choose a time to perform:
     $performerA_time = $times[array_rand($times)];
-//add performer A's to the schedule    
+//add performer A's time to the schedule    
     $schedule[] = $performerA_time;
-
-// Performer B is given a random time to perform:
+// For Performer B, randomly choose a time to perform:
     $performerB_time = $times[array_rand($times)];
-// compare to performer A's time to see if time is available
+// compare performer B's time to performer A's time to see if time is available
         if ($performerB_time != $performerA_time) { 
-// updated schedule to include PB if they don't match        
+// updated schedule to include PB if the time is available and announce when both bands will perform        
         $schedule[] = $performerB_time;
             echo "Rock and Roll! The Foos will play at  " . $performerA_time . " and The Bars will play at  " . $performerB_time ;
+// announce that the second band will not perform          
         } else {
                 echo "Wah-Wah is not just a pedal. The Bars will not be a performing today.";
             }
