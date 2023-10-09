c4a985fe235c93e0c0af046bfcca01d81080c524
Mary Bradley <mbradley@hbs.edu>
2023-10-09 17:59:42 +0000

    not adding to schedule.. try pop,etc.

--- p1/index.php
+++ p1/index.php
@@ -4,7 +4,9 @@
 ///+ itialize an array called schedule
 $schedule = 0;
 //+ Create an array of times to play with the times
-$times =[1,2,3,4,5];
+$times =[1,2,3];
+$performerA_time = 0;
+$performerB_time = 0;
 //var_dump($times);
 //+ For Performer A, randomly chose a time to play:
     $performerA_time = $times[array_rand($times)];
@@ -18,6 +20,22 @@ $times =[1,2,3,4,5];
   else {
     echo "Performer A should pick a new time  ";
    }
+
+// are there still time slots available?
+if($schedule >= 0) {
+    $performerB_time != $times[array_rand($times)];
+    // compare to schedule to see if time is available (copy code from $performerA)
+        if ($performerB_time != $schedule) {
+            // updated schedule if they don't match
+            $schedule = $performerB_time;
+          //  var_dump($performerA_time);
+            echo "Performer B scheduled for time:  " ,$schedule; 
+            var_dump($schedule);
+        }
+      else {
+        echo "Performer B should pick a new time  ";
+       }
+}
 
 // foreach ($times as $time) {
  //  if ($schedule = $time) {
