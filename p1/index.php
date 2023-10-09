dfc7a600f588c5deaec3ebcc326e55e471f18152
Mary Bradley <mbradley@hbs.edu>
2023-10-09 14:00:34 +0000

    stuck on pulling two elements from an array

--- p1/index.php
+++ p1/index.php
@@ -5,13 +5,15 @@ $schedule = ['band'. 'time'];
 //+ Create an array of band names with the names
 $band = ['The Woo', 'The Wah', 'The Wee', 'The Wink', 'Foo'];
 //+ Create an array of times to play with the times
-$times = [1,2,3,4,5];
+$times[0,4];
+var_dump($times);
 //+ For performer, randomly choose an element from the band names 
-$performer1 = $band[rand(0-4)];
-//+ For performer, randomly choose an element from the time to play array
-$performer1 = $times[rand(0-4)];
-
+$performer1 = $band[rand(0,4), $times[rand(0.4)];
 var_dump($performer1);
+//+ For performer, randomly choose an element from the time to play array
+//$performer1 = $times[rand(0-4)];
+
+//var_dump($performer1);
 //+ Compare Performer A's time to the integer element in the schedule array, if they don't match, pop the band name and time to array 
 
 //+else, tell Performer to pick a new time.
