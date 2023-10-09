ab173c99751ab984f64a0ae63b7e6ff986e53256
Mary Bradley <mbradley@hbs.edu>
2023-10-09 14:54:52 +0000

    Sched array initialized corrected /add back bands?

--- p1/index.php
+++ p1/index.php
@@ -1,23 +1,20 @@
 <?php
 ## Game planning
 //+ itialize an array called schedule for two elements, a string and an integer (band names and times)
-$schedule = ['band'. 'time'];
-//+ Create an array of band names with the names
-$band = ['The Woo', 'The Wah', 'The Wee', 'The Wink', 'Foo'];
-var_dump($band);
+$schedule = ["",'0'];
 //+ Create an array of times to play with the times
 $times =[0,4];
 var_dump($times);
 //+ For performer, randomly choose an element from the band names 
-$performer = $band[rand(0,4)];
+$performer = $times[rand(0,4)];
 var_dump($performer);
 //+ For performer, randomly choose an element from the time to play array
-$ = $times[rand(0,4)];
-var_dump($performer);
+//$ = $times[rand(0,4)];
+//var_dump($performer);
 //+ Compare Performer A's time to the integer element in the schedule array, if they don't match, pop the band name and time to array 
-if $performer[$time] != $schedule[1];
-$schedule = [$band, $times];
-else echo $performer[$time] "is not available.  Choose a new time';
+//if ($performer[$time]) != $schedule[1];
+  //  $schedule = [$band, $times];
+    //elseif ($performer[$time]) = $schedule[1]; echo //$performer[$time] "is not available.  Choose a new time';
 
 //+else, tell Performer to pick a new time.
 //+ if band name key >= 0 
