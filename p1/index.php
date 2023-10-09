b4872739d0d8a92c369d08b165f41fd0eff574ca
Mary Bradley <mbradley@hbs.edu>
2023-10-09 13:38:22 +0000

    first check, doesn't work

--- p1/index.php
+++ p1/index.php
@@ -1,33 +1,24 @@
 <?php
 ## Game planning
-+ itialize an array called schedule for two elements, a string and an integer (band names and times)
-+ Create an array of band names with the names
-+ Create an array of times to play with the times
-+ For performer, randomly choose an element from the band names 
-+ For performer, randomly choose an element from the time to play array
-+ Compare Performer A's time to the integer element in the schedule array, if they don't match, pop the band name and time to array 
-+else, tell Performer to pick a new time.
-+ if band name key >= 0 
-+ For performer, randomly choose an element from the band names 
-+ else Report band names and time they will play
+//+ itialize an array called schedule for two elements, a string and an integer (band names and times)
+$schedule = ['band'. 'time'];
+//+ Create an array of band names with the names
+$band = ['The Woo', 'The Wah', 'The Wee', 'The Wink', 'Foo'];
+//+ Create an array of times to play with the times
+$times = [1,2,3,4,5];
+//+ For performer, randomly choose an element from the band names 
+$performer1 = $band[rand(0-4)];
+//+ For performer, randomly choose an element from the time to play array
+$performer1 = $times[rand(0-4)];
+
+var_dump($performer1);
+//+ Compare Performer A's time to the integer element in the schedule array, if they don't match, pop the band name and time to array 
+
+//+else, tell Performer to pick a new time.
+//+ if band name key >= 0 
+//+ For performer, randomly choose an element from the band names 
+//+ else Report band names and time they will play
 
 
 
 require 'index-view.php';
-
-//comment out code
-# Define 4 different variables, which will
-# each represent how much a given coin is worth
-
-# $penny_value = .01;
-
-
-# Define 4 more variables, which will each
-# represent how many of each coin is in the bank
-//adding a comment
-
-# $pennies = 300;
-
-
-# Add up how much money is in the piggy bank
-#$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value) + ($hquarters * $hquarter_value);
