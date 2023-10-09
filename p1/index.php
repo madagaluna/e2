e5ec97524d7253121d0220bc08faa347ae325486
Mary Bradley <mbradley@hbs.edu>
2023-10-09 19:43:05 +0000

    added implode to create a schedule

--- p1/index.php
+++ p1/index.php
@@ -2,7 +2,7 @@
 ## Game planning
 
 ///+ itialize an array called schedule
-$schedule = 0;
+$schedule = [];
 //+ Create an array of times to play with the times
 $times =[1,2,3];
 $performerA_time = 0;
@@ -10,26 +10,20 @@ $performerB_time = 0;
 //var_dump($times);
 //+ For Performer A, randomly choose a time to play:
     $performerA_time = $times[array_rand($times)];
-// compare to schedule to see if time is available
-    if ($performerA_time != $schedule) {
-        // updated schedule if they don't match
-        $schedule = $performerA_time;
-      //  var_dump($performerA_time);
-       echo "Performer A scheduled for time:  " ,$schedule; 
-    }
-  else {
-    echo "Performer A should pick a new time  ";
-   }
+//add performer A's to the schedule    
+    $schedule[] = $performerA_time;
 
 // Performer B is given a random time to perform:
     $performerB_time = $times[array_rand($times)];
-    echo "performer B is scheduled for ";
-    var_dump($performerB_time);
-    // compare to performer A's time to see if time is available
+// compare to performer A's time to see if time is available
         if ($performerB_time != $performerA_time) { 
-            echo "Rock and Roll! Performer A plays at  " . $performerA_time . " and Performer B plays at  " . $performerB_time ;
+// updated schedule to include PB if they don't match        
+        $schedule[] = $performerB_time;
+            echo "Rock and Roll! The Foos will play at  " . $performerA_time . " and The Bars will play at  " . $performerB_time ;
         } else {
-                echo "Wah-Wah is not just a pedal. performance today Performer B.";
+                echo "Wah-Wah is not just a pedal. The Bars will not be a performing today.";
             }
-
-// require 'index-view.php';
+// print the schedule https://www.w3schools.com/php/func_string_implode.asp
+echo "  There will be music at:  ". implode(" , ", $schedule);
+        
+// require 'index-view.php'
