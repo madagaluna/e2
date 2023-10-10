ef52d7582926050e932f2289c1361279202e49bf
Mary Bradley <mbradley@hbs.edu>
2023-10-10 00:22:37 +0000

    cleaned up -view

--- p1/index.php
+++ p1/index.php
@@ -5,11 +5,9 @@ $results = [];
 for ($i = 1; $i <=10; $i++) {
 //+ initalize an array called schedule
 $schedule = [];
-//+ Create an array of times to play with the times
+//+ Create an array of available performance times
 $times =[1,2,3];
-//defining the performer variables
-//$performerA_time = 0;
-//$performerB_time = 0;
+
 //+ For Performer A, randomly choose a time to perform:
     $performerA_time = $times[array_rand($times)];
 //add performer A's time to the schedule    
@@ -18,16 +16,15 @@ $times =[1,2,3];
     $performerB_time = $times[array_rand($times)];
 // compare performer B's time to performer A's time to see if time is available
         if ($performerB_time != $performerA_time) { 
-// updated schedule to include PB if the time is available and announce when both bands will perform  this redundant now that I'm doing the results but I like it.  
+// updated schedule to include PB if the time is available  
         $schedule[] = $performerB_time; }
-//           echo "Rock and Roll! The Foos will perform at  " . $performerA_time . " and The Bars will perform at  " . $performerB_time;
-// announce that The Bars will not perform - maybe just keep this         
+// announce that The Bars will not perform        
         else {
 
                $schedule[] = "Wah-Wah is not just a pedal. The Bars will not be a performing today.";
            }
 
-// print the schedule https://www.w3schools.com/php/func_string_implode.asp
+// concatenate the schedule and make it a string https://www.w3schools.com/php/func_string_implode.asp
   $schedule_String =  implode(" , ", $schedule);
 //show results
 
