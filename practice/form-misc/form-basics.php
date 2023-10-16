71733022f567b9d5685ef24cdf0d5eb88ae014ca
Mary Bradley <mbradley@hbs.edu>
2023-10-16 01:00:22 +0000

    week 6 code for forms

---
+++ practice/form-misc/form-basics.php
@@ -0,0 +1,45 @@
+// include HTML basics up here
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Schedule Sign Up</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+
+
+<form action='signup.php'>
+    <div>
+        <label for='name'>Name</label>
+        <input type='text' id='name' value='Susan Buck'>
+    </div>
+    <div>
+        <label for='email'>E-mail</label>
+        <input type='email' id='email'>
+    </div>
+    <div>
+        <label for='password'>Password</label>
+        <input type='password' id='password'>
+    </div>
+    <div>
+        <label for='source'>How did you hear about us?</label>
+        <select id='source'>
+            <option>Choose one...</option>
+            <option selected>Search engine</option>
+            <option>From a friend/family member</option>
+            <option>From an advertisement</option>
+        </select>
+    </div>
+    <div>
+        <label for='bio'>Short bio</label>
+        <textarea id='bio'>This is the default bio...</textarea>
+    </div>
+    <div class='form-group form-check'>
+        <input type='checkbox' id='subscribe' checked>
+        <label for='subscribe'>Subscribe to our newsletter</label>
+    </div>
+    <button type='submit'>Sign up</button>
+</form>
+
+</html>
---
+++ practice/process.php
@@ -0,0 +1,15 @@
+<?php
+
+$answer = $_POST['answer'];
+
+$haveAnswer = true;
+
+if ($answer == ''){ //left blank
+    $haveAnswer = false;
+} elseif (answer == 'pumpkin')  {// correct answer//
+    $correct = true;
+} else {
+    $correct = false;
+}
+
+require 'process-view.php'
