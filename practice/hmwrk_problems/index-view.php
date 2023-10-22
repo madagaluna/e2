2511a77f73d1a7b1bcb8fca0c5302b3173bacf5f
Mary Bradley <mbradley@hbs.edu>
2023-10-22 15:17:21 +0000

    hmwrk

--- practice/hmwrk_problems/index-view.php
+++ practice/hmwrk_problems/index-view.php
@@ -2,7 +2,7 @@
 <html lang='en'>
 
 <head>
-    <title>Week 7</title>
+    <title>Week 7 homeword practice</title>
     <meta charset='utf-8'>
     <link href=data: , rel=icon>
 </head>
@@ -10,7 +10,21 @@
 <body>
 
     <h1> Week 7 Practice </h1>
+    <h1>Rock or Roll</h1>
+    <h2>Game Mechanics</h2>
+    <p> fUN WITH fUNCTIONS ... PUTTING THE FUNC INTO FUNCTIONS </p>
 
+    <h2>Results</h2>
+    <?php foreach($results as $result) { ?>
+    <ul>
+        <li>The Foos were signed up for <?php echo $result['the_Foos_time'] ?>
+        </li>
+        <li>The Bars were signed up for <?php echo $result['the_Bars_time'] ?>
+        </li>
+        <li>The performance time(s) will be: <?php echo $result['the_Schedule_is'] ?>
+        </li>
+    </ul>
+    <?php } ?>
 </body>
 
 </html>
--- practice/hmwrk_problems/index.php
+++ practice/hmwrk_problems/index.php
@@ -1,13 +1,41 @@
-session_start();
-
-if (isset($_SESSION['results'])) { // so you are only looking at the curernt session use if and isset instead of
-!is_null($_Session)etc !is_null doesn't work cuz the rest still displays)
-$results = $_SESSION['results'];
-
-$haveAnswer = $results['haveAnswer'];
-$correct = $results['correct'];
-
-$_SESSION['results'] = null;
+<?php
+## Game planning
+// initalize an array to hold results 
+## adding fx
+function availabletimes($performerA_time, $performerB_time) {
+    return $performerA_time != $performerA_time;
+$results = [];
 }
+for ($i = 1; $i <=10; $i++) {
+// initalize an array called schedule
+$schedule = [];
+// Create an array of available performance times
+$times =[1,2,3];
 
+// For Performer A, randomly choose a time to perform:
+    $performerA_time = $times[array_rand($times)];
+//add performer A's time to the schedule    
+    $schedule[] = $performerA_time;
+// For Performer B, randomly choose a time to perform:
+    $performerB_time = $times[array_rand($times)];
+// compare performer B's time to performer A's time to see if time is available
+        if (availabletimes($performerB_time, $performerA_time)) { 
+// if the time is available, update schedule to include PB  
+        $schedule[] = $performerB_time; }
+// else update schedule that The Bars will not be performing        
+        else {
+
+               $schedule[] = "that's it: Wah-Wah is not just a pedal. The Bars will not be a performing today.";
+           }
+
+// concatenate the schedule and make it a string https://www.w3schools.com/php/func_string_implode.asp
+  $schedule_String =  implode(" and ", $schedule);
+//show results
+
+   $results [] = [
+    'the_Foos_time' => $performerA_time,
+    'the_Bars_time' => $performerB_time,
+    'the_Schedule_is' => $schedule_String,
+   ];    
+}
 require 'index-view.php';
