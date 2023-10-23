db5a47d46730b4d488dddbe1d28a5320c4ed5249
Mary Bradley <mbradley@hbs.edu>
2023-10-23 00:44:46 +0000

    vowel hmwrk

--- practice/hmwrk_problems/index.php
+++ practice/hmwrk_problems/index.php
@@ -7,7 +7,8 @@ function availabletimes($performerA_time, $performerB_time) {
 
 $results = [];
 }
-for ($i = 1; $i <=10; $i++) {
+
+    for ($i = 1; $i <=10; $i++) {
 // initalize an array called schedule
 $schedule = [];
 // Create an array of available performance times
@@ -25,7 +26,6 @@ $times =[1,2,3];
         $schedule[] = $performerB_time; }
 // else update schedule that The Bars will not be performing        
         else {
-
                $schedule[] = "that's it: Wah-Wah is not just a pedal. The Bars will not be a performing today.";
            }
 
@@ -39,4 +39,41 @@ $times =[1,2,3];
     'the_Schedule_is' => $schedule_String,
    ];    
 }
+
+/*Write a function that will produce a count of how many vowels are in a given word.
+The function should be called vowelCount
+It should accept 1 parameter, $word
+It should return an integer value (the vowel count) */
+
+
+function checkNumber($guess, $mysteryNumber)
+{
+    if ($guess == $mysteryNumber) {
+        return 'correct';
+    } elseif ($guess > $mysteryNumber) {
+        return 'high';
+    } else {
+        return 'low';
+    }
+}
+
+// var_dump(checkNumber(3, 6)); # low
+// var_dump(checkNumber(7, 3)); # high
+var_dump(checkNumber(3, 3)); # correct
+
+
+
+foreach($cards as $key => $number) {
+    if ($key % 2 == 0);
+   // echo 'used https://www.geeksforgeeks.org/php-check-number-even-odd/' - then saw it in e2 notes!;
+       $playerCards[] = array_shift($cards); 
+       if ($key % 2 != 0);
+       $computerCards [] = array_shift($cards);
+    if($key >= 4) {
+       break;
+   } 
+}
+
+
+
 require 'index-view.php';
