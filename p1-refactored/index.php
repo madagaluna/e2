ddbeb0788946470ee4630fdd50ef3179d40190a3
Mary Bradley <mbradley@hbs.edu>
2023-10-23 03:25:55 +0000

    more to do - ternary

--- p1-refactored/index.php
+++ p1-refactored/index.php
@@ -1,13 +1,12 @@
 <?php
 ## Game planning
-
 function availabletimes($performerA_time, $performerB_time) {
     return $performerA_time != $performerB_time;
 $results = [];}
     for ($i = 1; $i <=10; $i++) {
 $schedule = [];
 $times =[1,2,3];
-    $performerA_time = $times[array_rand($times)];
+    $performerA_time = $times[array_rand($times)];  # apply ternary here!
     $schedule[] = $performerA_time;
     $performerB_time = $times[array_rand($times)];
         if (availabletimes($performerB_time, $performerA_time)) { 
