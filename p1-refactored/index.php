3924d21bb6392e1096b81a79ad434721a811ebd2
Mary Bradley <mbradley@hbs.edu>
2023-10-24 16:53:06 +0000

    ternary added

--- p1-refactored/index.php
+++ p1-refactored/index.php
@@ -6,18 +6,22 @@ $results = [];}
     for ($i = 1; $i <=10; $i++) {
 $schedule = [];
 $times =[1,2,3];
-    $performerA_time = $times[array_rand($times)];  # apply ternary here!
+    $performerA_time = $times[array_rand($times)];  
     $schedule[] = $performerA_time;
     $performerB_time = $times[array_rand($times)];
-        if (availabletimes($performerB_time, $performerA_time)) { 
-        $schedule[] = $performerB_time; }     
-        else {
-               $schedule[] = "that's it: Wah-Wah is not just a pedal. The Bars will not be a performing today.";
-           }
+  #      if (availabletimes($performerB_time, $performerA_time)) { # apply ternary here!
+   #     $schedule[] = $performerB_time; }     
+  ##             $schedule[] = "that's it: Wah-Wah is not just a pedal. The Bars will not be a performing today.";
+   #        }
+
+    $schedule[] = 
+        $availabletimes($performerA_time, $performerB_time) ? $performerB_time : "that's all for today: Wah-Wah is not just a pedal. The Bars will not be a performing today.";
+   
   $schedule_String =  implode(" and ", $schedule);
    $results [] = [
     'the_Foos_time' => $performerA_time,
     'the_Bars_time' => $performerB_time,
     'the_Schedule_is' => $schedule_String,
-   ];    
+   ]; 
 }
+require 'index-view.php';
