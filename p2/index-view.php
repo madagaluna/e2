d943583be043e751b5f1eaa509b5b727946906c3
Mary Bradley <mbradley@hbs.edu>
2023-10-28 18:57:52 +0000

    labels, not yet

--- p2/index-view.php
+++ p2/index-view.php
@@ -12,17 +12,15 @@
     <h2> Project 2 - Rock and Roll </h2>
 
     <form method='POST' action='process.php'>
-        <!-- dont need values to be exposed, so  use post, action is where to submit the form to -->
-        <!-- on a different page you have    = the array is populated by  the value-->
-        <!-- ?? = the value, heads or tail from the name ('choice') array 
-                created a ternary php island to have the radial button checked based on previous choice-->
-        <input type='radio' id='heads' name='choice' value='heads'
-            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>><label for='heads'>heads</label>
-        <!-- change to dropdown or text -->
-        <!-- to make the two radio buttons the same, the name is the same (the name data is used only when getting data during submission - the Postsuperglobal will have the array with all the data, it will be keyed/indexed based on the name "choice"), match the lable to the ID, added checked on heads to make it the default choice-->
-        <input type='radio' id='tails' name='choice' value='tails'
-            <?php echo (isset($choice) and $choice == 'tails') ? 'checked' : '' ?>>
-        <label for='tails'>tails</label><!-- to make the two -->
+        <input type=text id='perform-time' name='choice' value='1'
+            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
+        <label for='perform-time'>Enter a time (1,2, or 3):</label>
+        <input type=text id='perform-time' name='choice' value='2'
+            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
+        <label for='perform-time'>Enter a time (1,2, or 3):</label>
+        <input type=text id='perform-time' name='choice' value='3'
+            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
+        <label for='perform-time'>Enter a time (1,2, or 3):</label>
         <button type='submit'>Submit</button>
         `
     </form>
