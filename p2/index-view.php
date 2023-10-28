64aa59caf49cf0e6c2bf5dd1524599337787a63f
Mary Bradley <mbradley@hbs.edu>
2023-10-28 20:16:13 +0000

    closer to perform time label

--- p2/index-view.php
+++ p2/index-view.php
@@ -12,16 +12,14 @@
     <h2> Project 2 - Rock and Roll </h2>
 
     <form method='POST' action='process.php'>
-        <input type=text id='perform-time' name='choice' value='1'
-            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
-        <label for='perform-time'>Enter a time (1,2, or 3):</label>
-        <input type=text id='perform-time' name='choice' value='2'
-            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
-        <label for='perform-time'>Enter a time (1,2, or 3):</label>
-        <input type=text id='perform-time' name='choice' value='3'
-            <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
-        <label for='perform-time'>Enter a time (1,2, or 3):</label>
-        <button type='submit'>Submit</button>
+        <input type='radio' name="perform_ time" id="1pm" value="1"><label for='1pm'>
+            <input type='radio' name="perform_ time" id="2pm" value="2"><label for='2pm'>
+                <input type='radio' name="perform_ time" id="3pm" value="3"><label for='3pm'>
+
+                    <?php echo (!isset($choice) or $choice == 'heads') ? 'checked' : ''; ?>>
+                    <label for='perform-time'>Enter a time (1,2, or 3):</label>
+
+                    <button type='submit'>Submit</button>
 
     </form>
 
