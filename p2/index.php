fa9375db0291fa45fcf3a6a10c925670cc16a741
Mary Bradley <mbradley@hbs.edu>
2023-10-28 16:36:42 +0000

    Week 8 example

--- p2/index.php
+++ p2/index.php
@@ -1,3 +1,20 @@
 <?php
 
+session_start();
+
+if(isset($_SESSION['results'])) {
+    // accesing the session results 3 times so turn the sessionresults into a variable $results instead of a $SESSION['results']
+    $results = $_SESSION['results'];
+    $winner = $results['winner'];
+    $flip = $results['flip'];
+    $choice = $results['choice'];
+    //  var_dump($winner);
+    //   var_dump($flip);
+    //  var_dump($choice);
+
+    $_SESSION['results'] = null; // to clear results - works by isset evaluating to false.
+}
+
+
+
 require "index-view.php";
