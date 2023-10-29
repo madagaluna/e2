227c00e80e4e0f7ea1541beb8ca7355a927ffde6
Mary Bradley <mbradley@hbs.edu>
2023-10-29 03:18:57 +0000

    var_dump only visible after no time selected

--- p2/process.php
+++ p2/process.php
@@ -1,18 +1,19 @@
 <?php
 
 session_start();  // the data has to be available when user is redirected back - create the
-//var_dump($_POST);
-
-
+var_dump($_POST);
 
 if (isset($_POST['perform_time'])) {
     $perform_time = $_POST['perform_time'];
     $schedule = [];
     $performer_A = (isset($_SESSION['performer_A'])) ? $_SESSION ['performer_A'] : rand(1, 3);
 
-    $schedule[] = ($performer_A != $perform_time) ?
-    $performer_A . "PM \n" . $perform_time :
-    "The Foos are playing at " . $performer_A . "PM Please choose another time.";
+    if ($performer_A != $perform_time) {
+        $schedule[] = $performer_A;
+        $schedule[] = $perform_time;
+    } else {
+        "The Foos are playing at " . $performer_A . "PM Please choose another time.";
+    }
 
     $_SESSION['results'] =  [
         'schedule' => $schedule,
