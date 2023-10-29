25b7b5a920fc1221f9e2fb1d67943a9dec911fdd
Mary Bradley <mbradley@hbs.edu>
2023-10-29 17:48:42 +0000

    seesion global null prevents results from showing

--- p2/index.php
+++ p2/index.php
@@ -3,16 +3,15 @@
 session_start();
 if(isset($_SESSION['results'])) {
 
-    //     // //if (isset($_SESSION['results'])) {
+
 
     $results = $_SESSION['results'];
     $perform_time = $results['selected_time'];
     $taken = $results['taken_time'];
     $avail = $results['available_time'];
 
-    $_SESSION['result'] = null;
-
+    $_SESSION['results'] = null;
 }
 
-// // $_SESSION['results'] = null;
+
 require "index-view.php";
