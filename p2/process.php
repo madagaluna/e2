c4952b34b74950a758f109ad9b904050c831883f
Mary Bradley <mbradley@hbs.edu>
2023-10-29 03:13:27 +0000

    form results with warning, unk if redo wrks

--- p2/process.php
+++ p2/process.php
@@ -3,18 +3,22 @@
 session_start();  // the data has to be available when user is redirected back - create the
 //var_dump($_POST);
 
-$schedule = [];
+
 
 if (isset($_POST['perform_time'])) {
     $perform_time = $_POST['perform_time'];
-    $performer_A = rand(1, 3);
+    $schedule = [];
+    $performer_A = (isset($_SESSION['performer_A'])) ? $_SESSION ['performer_A'] : rand(1, 3);
 
     $schedule[] = ($performer_A != $perform_time) ?
     $performer_A . "PM \n" . $perform_time :
     "The Foos are playing at " . $performer_A . "PM Please choose another time.";
 
-    $_SESSION['schedule'] = $schedule;
+    $_SESSION['results'] =  [
+        'schedule' => $schedule,
+        'The Foos' => $performer_A,
+        'The Bars' => $perform_time,
 
-
+    ];
     header('Location: index.php');
 }
