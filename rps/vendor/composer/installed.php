19d73906814c1faa11b9eebb9038c22325ece885
Mary Bradley <mbradley@hbs.edu>
2023-11-03 23:48:48 +0000

    Composer installed, package pulled in

---
+++ rps/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fad4874d7a950b429a22f6be5c413b39d2b73482',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fad4874d7a950b429a22f6be5c413b39d2b73482',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'susanbuck/rock-paper-scissors' => array(
+            'pretty_version' => '0.1.6',
+            'version' => '0.1.6.0',
+            'reference' => 'c198dd8b5b7d8d0be6471ea310ee41ba16527ce7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../susanbuck/rock-paper-scissors',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
