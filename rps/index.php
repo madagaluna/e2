c19cf0b2a4e4e211b40575fab4b9ea03afb853ed
Mary Bradley <mbradley@hbs.edu>
2023-11-04 00:05:11 +0000

    RPS basics from class

--- rps/index.php
+++ rps/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Rock, Paper Scissors';
+require __DIR__ . '/vendor/autoload.php';  # pulls in autoload from autoload dir - pulls in all referenced classes
+
+use RPS\Game;  #imports from namespace / references the class Game from RPS/ namespace
+use App\Debug; #invoking thoughout file
+
+$game = new Game();  #new instance
+
+# Each invocation of the `play` method will play and track a new round of player (given move) vs. computer
+Debug::dump($game->play('rock'));  #invoking play method
