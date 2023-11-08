f6840bf66f6dd3fc6c00693e34a7d04d9df84a03
Mary Bradley <mbradley@hbs.edu>
2023-11-08 21:44:44 +0000

    fixing hmwrk.

--- practice/homework_9/MyGame.php
+++ practice/homework_9/MyGame.php
@@ -2,22 +2,28 @@
 
 namespace RPS;
 
+//use RPS\Game;  // Susam has this ... also she had MyGame in Class folder (app)
+
 class MyGame extends Game
 {
-    protected $moves = ['heads', 'tails'];
+    protected $moves = ['heads', 'tails'];  // protected is used within the same class and overwrites  so heads and tails instead of RPS - when class is protected
 
     /**
      * Compares $playerMove against $computerMove and
      * determines whether the player won or lost (no tie)
      */
+
     protected function determineOutcome($playerMove, $computerMove)
     {
-        if ($playerMove == $computerMove) {
-            return 'lost';
-        } elseif ($playerMove == 'heads' && $computerMove == 'tails' || $playerMove == 'tails' && $computerMove == 'heads') {
-            return 'won';
-        } else {
-            return 'invalid';
-        }
+        return $computerMove == $playerMove;
     }
+    //        if ($playerMove == $computerMove) {
+    ///          return 'lost';
+    //       } elseif ($playerMove == 'heads' && $computerMove == 'tails' || $playerMove == 'tails' && $computerMove == 'heads') {
+    //          return 'won';
+    //       } else {
+    //            return 'invalid';
+    //       }
+    //   }
+
 }
